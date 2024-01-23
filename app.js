f2a0023462c56f396f93793c745ee14dc6d442dd
Martin Fernandez <Electronicsmf@aol.com>
2024-01-22 20:18:54 -0500

    add es6 code to project

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var images = document.querySelectorAll("img");
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll("img");
 
-    images.forEach(function(img) {
-        img.addEventListener("mouseover", function() {
-            this.style.transition = "width 1s, opacity 1s";
-            this.style.width = "175px";
-            this.style.opacity = 1;
+    images.forEach(img => {
+        img.addEventListener("mouseover", () => {
+            img.style.transition = "width 1s, opacity 1s";
+            img.style.width = "175px";
+            img.style.opacity = 1;
         });
 
-        img.addEventListener("mouseout", function() {
-            this.style.transition = "width 1s, opacity 1s";
-            this.style.width = "100px";
-            this.style.opacity = 0.3;
+        img.addEventListener("mouseout", () => {
+            img.style.transition = "width 1s, opacity 1s";
+            img.style.width = "100px";
+            img.style.opacity = 0.3;
         });
     });
 });
